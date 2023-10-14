e05fbcfeee2a8c0a30645298407ac1042b033099
eremeydack <139877570+eremeydack@users.noreply.github.com>
2023-10-14 20:41:11 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,72 +1,55 @@
-def generate_random_data():
-if __name__ == "__main__":
-    main()
-def main():
     return data
+def main():
+    main()
+    for item in data:
+if __name__ == "__main__":
 
-    data = generate_random_data()
+def generate_random_data():
 
 import random
 
-        print(f"Random Number: {item}")
+
     data = [random.randint(1, 100) for _ in range(10)]
-
-    for item in data:        console.log("Random Number: " + item);
-        data.push(Math.floor(Math.random() * 100) + 1);
-
-
-main();
-    }
-    data.forEach(item => {
-}
-    });
-}
-    for (let i = 0; i < 10; i++) {
-function generateRandomData() {
-    const data = [];
-
-function main() {
-    return data;
-    const data = generateRandomData();    return data
-def generate_random_data():
-if __name__ == "__main__":
-def main():
-
-
-
-    data = generate_random_data()
-
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
         print(f"Random Number: {item}")
-    main()
-
-    data = generate_random_data()
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
+    data = generate_random_data()def main():
     for item in data:
 
-    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
     return data
+
+
+import random
 def generate_random_data():
+        print(f"Random Number: {item}")
+
     main()
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]    for item in data:
+    main()
+    return data
 def main():
 
-import random    return data;
-    const data = generateRandomData();
-        data.push(Math.floor(Math.random() * 100) + 1);
-    }
-function generateRandomData() {
+    data = [random.randint(1, 100) for _ in range(10)]
 
+        print(f"Random Number: {item}")
+def generate_random_data():
 
+    data = generate_random_data()
+
+if __name__ == "__main__":
+import random    const data = generateRandomData();
         console.log("Random Number: " + item);
     data.forEach(item => {
+    }
+
 }
+
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
 }
     for (let i = 0; i < 10; i++) {
+    return data;
     });
-function main() {
-main();
-
     const data = [];
+main();
