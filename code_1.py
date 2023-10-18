cf3a4158946f39386004bcb70a900d4a6b8c59b9
eremeydack <139877570+eremeydack@users.noreply.github.com>
2023-10-18 17:05:52 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,30 +1,26 @@
-if __name__ == "__main__":
-def generate_random_data():
-    return data
+
 
     main()
-import random
-def main():
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-
+    data = generate_random_data()
     for item in data:
 
+    return data
+import random
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
 
-    data = generate_random_data()main();
-    return data;
-    const data = generateRandomData();
+def generate_random_data():
+        for (int item : data) {
+        }
 
-
-    data.forEach(item => {
-        data.push(Math.floor(Math.random() * 100) + 1);
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
     }
+        for (int i = 0; i < 10; i++) {
+        }
 }
-function main() {
-    for (let i = 0; i < 10; i++) {
-function generateRandomData() {
-        console.log("Random Number: " + item);
-}
-    const data = [];
-
-    });
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
